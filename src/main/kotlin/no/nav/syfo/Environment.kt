410c8c122818196bfd61bd26f12241ceaf7a6ba7
Joakim kartveit <joakim.kartveit@nav.no>
2018-09-03 08:47:59 +0200

    La til ein del fasit ressurser

--- src/main/kotlin/no/nav/syfo/Environment.kt
+++ src/main/kotlin/no/nav/syfo/Environment.kt
@@ -6,16 +6,15 @@ data class Environment(
     val srvsyfosmpapirmottakUsername: String = getEnvVar("SRVSYFOSMPAPIRMOTTAK_USERNAME"),
     val srvsyfosmpapirmottakPassword: String = getEnvVar("SRVSYFOSMPAPIRMOTTAK__PASSWORD"),
     val kafkaBootstrapServers: String = getEnvVar("KAFKA_BOOTSTRAP_SERVERS_URL"),
-    val mqQueueManagerName: String = getEnvVar("MQGATEWAY04_NAME"),
-    val mqHostname: String = getEnvVar("MQGATEWAY04_HOSTNAME"),
-    val mqPort: Int = getEnvVar("MQGATEWAY04_PORT").toInt(),
-    val mqGatewayName: String = getEnvVar("MQGATEWAY04_NAME"),
+    val mqQueueManagerName: String = getEnvVar("MQGATEWAY03_NAME"),
+    val mqHostname: String = getEnvVar("MQGATEWAY03_HOSTNAME"),
+    val mqPort: Int = getEnvVar("MQGATEWAY03_PORT").toInt(),
     val mqChannelName: String = getEnvVar("SYFOSMPAPIRMOTTAK"),
     val srvappserverUsername: String = getEnvVar("SRVAPPSERVER_USERNAME", "srvappserver"),
     val srvappserverPassword: String = getEnvVar("SRVAPPSERVER_PASSWORD", ""),
-    val syfosmpapirmottakinputQueueName: String = getEnvVar("SYFSMPAPIROMOTTAK_INPUT_QUEUE_QUEUENAME"),
+    val syfosmpapirmottakinputQueueName: String = getEnvVar("SYFSMPAPIROMOTTAK_INPUT_QUEUENAME"),
     val kafkaSM2013PapirmottakTopic: String = getEnvVar("KAFKA_SM2013_PAPIR_MOTTAK_TOPIC", "privat-syfosmpapirmottak-sm2013"),
-    val syfomottakinputBackoutQueueName: String = getEnvVar("SYFOMOTTAK_BACKOUT_QUEUE_QUEUENAME")
+    val syfomottakinputBackoutQueueName: String = getEnvVar("SYFOMOTTAK_BACKOUT_QUEUENAME")
 )
 
 fun getEnvVar(varName: String, defaultValue: String? = null) =
