6be0d8bdde1a6e88e1853e4263aef8ec8a9210a9
Joakim kartveit <joakim.kartveit@nav.no>
2019-02-01 09:43:38 +0100

    fixed formting issue with kotliner

--- src/main/kotlin/no/nav/syfo/model/Journalpost.kt
+++ src/main/kotlin/no/nav/syfo/model/Journalpost.kt
@@ -3,47 +3,47 @@ package no.nav.syfo.model
 // model: https://confluence.adeo.no/pages/viewpage.action?pageId=287444683
 
 data class Journalpost(
-        val journalTilstand: JournalTilstand,
-        val avsender: Avsender,
-        val brukerListe: List<Bruker>,
-        val arkivSak: ArkivSak,
-        val tema: String,
-        val tittel: String,
-        val kanalReferanseId: String?,
-        val forsendelseMottatt: String,
-        val mottaksKanal: String?,
-        val journalfEnhet: String?,
-        val dokumentListe: List<Dokument>
+    val journalTilstand: JournalTilstand,
+    val avsender: Avsender,
+    val brukerListe: List<Bruker>,
+    val arkivSak: ArkivSak,
+    val tema: String,
+    val tittel: String,
+    val kanalReferanseId: String?,
+    val forsendelseMottatt: String,
+    val mottaksKanal: String?,
+    val journalfEnhet: String?,
+    val dokumentListe: List<Dokument>
 )
 
 data class Dokument(
-        val dokumentId: String,
-        val dokumentTypeId: String,
-        val navSkjemaId: String?,
-        val tittel: String,
-        val dokumentKategori: String,
-        val variant: List<Variant>,
-        val logiskVedleggListe: List<LogiskVedlegg>
+    val dokumentId: String,
+    val dokumentTypeId: String,
+    val navSkjemaId: String?,
+    val tittel: String,
+    val dokumentKategori: String,
+    val variant: List<Variant>,
+    val logiskVedleggListe: List<LogiskVedlegg>
 )
 
 data class LogiskVedlegg(
-        val logiskVedleggId: String,
-        val logiskVedleggTittel: String
+    val logiskVedleggId: String,
+    val logiskVedleggTittel: String
 )
 
 data class Variant(
-        val arkivFilType: String,
-        val variantFormat: String
+    val arkivFilType: String,
+    val variantFormat: String
 )
 
 data class ArkivSak(
-        val arkivSakSystem: String,
-        val arkivSakId: String
+    val arkivSakSystem: String,
+    val arkivSakId: String
 )
 
 data class Bruker(
-        val brukerType: BrukerType,
-        val identifikator: String?
+    val brukerType: BrukerType,
+    val identifikator: String?
 )
 
 enum class BrukerType {
@@ -55,9 +55,9 @@ enum class JournalTilstand {
 }
 
 data class Avsender(
-        val navn: String,
-        val avsenderType: AvsenderType,
-        val identifikator: String
+    val navn: String,
+    val avsenderType: AvsenderType,
+    val identifikator: String
 )
 
 enum class AvsenderType {
