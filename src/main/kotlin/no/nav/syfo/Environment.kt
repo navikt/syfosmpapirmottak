892c705bffd3cdf8487603576831eff92781a55c
Joakim Kartveit <joakimkartveit@gmail.com>
2019-06-07 13:51:19 +0200

    Fjernet saf,helsepersonell og kuhrsar url

--- src/main/kotlin/no/nav/syfo/Environment.kt
+++ src/main/kotlin/no/nav/syfo/Environment.kt
@@ -13,11 +13,8 @@ data class Environment(
     val securityTokenServiceUrl: String = getEnvVar("SECURITY_TOKEN_SERVICE_URL"),
     val arbeidsfordelingV1EndpointURL: String = getEnvVar("ARBEIDSFORDELING_V1_ENDPOINT_URL"),
     val journalfoerInngaaendeV1URL: String = getEnvVar("JOURNAL_FOER_INNGAAENDE_V1_URL"),
-    val safURL: String = getEnvVar("SAF_URL"),
     val stsURL: String = getEnvVar("STS_URL"),
     val aktoerregisterV1Url: String = getEnvVar("AKTORREGISTER_V1_URL"),
-    val helsepersonellv1EndpointURL: String = getEnvVar("HELSEPERSONELL_V1_ENDPOINT_URL"),
-    val kuhrSarApiUrl: String = getEnvVar("KUHR_SAR_API_URL", "http://kuhr-sar-api"),
     val oppgavebehandlingUrl: String = getEnvVar("OPPGAVEBEHANDLING_URL")
 ) : KafkaConfig
 
