1f9768f0fcce9fd0de5bf41a6f2041095e8db75c
Joakim Kartveit <joakimkartveit@gmail.com>
2019-06-18 09:18:42 +0200

    Fjernet ubrukt import

--- src/main/kotlin/no/nav/syfo/Bootstrap.kt
+++ src/main/kotlin/no/nav/syfo/Bootstrap.kt
@@ -3,7 +3,6 @@ package no.nav.syfo
 import com.ctc.wstx.exc.WstxException
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
