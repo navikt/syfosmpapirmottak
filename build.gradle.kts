ca4e1be37014197d1489c94573d5a8f53c253c63
Tia Firing <tia.firing@nav.no>
2019-09-03 10:30:11 +0200

    retter oidc-feil (syfosmcommon)

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ val jaxwsApiVersion = "2.3.1"
 val javaxAnnotationApiVersion = "1.3.2"
 val jaxbRuntimeVersion = "2.4.0-b180830.0438"
 val jaxwsToolsVersion = "2.3.1"
-val smCommonVersion = "2019.09.02-02-36-c9fb7fc524d827079666ed1574b55968aef68909"
+val smCommonVersion = "2019.09.03-10-15-c63a243d0ddbaf03cc98e788fc87052b16119b48"
 val javaxJaxwsApiVersion = "2.2.1"
 
 
