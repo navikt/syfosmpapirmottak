96a8aebbcceaae5ca8cd8bf9ae0d728ff42fb047
Joakim Kartveit <joakimkartveit@gmail.com>
2020-03-20 16:08:24 +0100

    Legger til try catch

--- src/main/kotlin/no/nav/syfo/service/SykmeldingService.kt
+++ src/main/kotlin/no/nav/syfo/service/SykmeldingService.kt
@@ -134,6 +134,7 @@ class SykmeldingService constructor(
                 }
             }
 
+            try {
                 val sakId = sakClient.finnEllerOpprettSak(sykmeldingsId = sykmeldingId, aktorId = aktorId, loggingMeta = loggingMeta)
 
                 val oppgave = oppgaveService.opprettOppgave(aktoerIdPasient = aktorId, sakId = sakId,
@@ -147,6 +148,9 @@ class SykmeldingService constructor(
                     )
                     PAPIRSM_OPPGAVE.inc()
                 }
+            } catch (e: Exception) {
+                log.warn("Noe gikk galt ved sak eller oppgave: ${e.message}, {}", fields(loggingMeta))
+            }
         }
     }
 
