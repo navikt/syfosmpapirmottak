c88893e79db9d554d0eb83570bae62e590be0d4e
Mathias Hellevang <mathias.hellevang@gmail.com>
2020-06-17 11:31:01 +0200

    Endret datoOpprettet i PapirSmRegistering fra LocalDate til OffsetDateTime

--- src/main/kotlin/no/nav/syfo/domain/PapirSmRegistering.kt
+++ src/main/kotlin/no/nav/syfo/domain/PapirSmRegistering.kt
@@ -1,7 +1,5 @@
 package no.nav.syfo.domain
 
-import java.time.LocalDate
-import java.time.LocalDateTime
 import no.nav.syfo.model.Arbeidsgiver
 import no.nav.syfo.model.Behandler
 import no.nav.syfo.model.KontaktMedPasient
@@ -10,27 +8,29 @@ import no.nav.syfo.model.MeldingTilNAV
 import no.nav.syfo.model.Periode
 import no.nav.syfo.model.Prognose
 import no.nav.syfo.model.SporsmalSvar
+import java.time.LocalDate
+import java.time.OffsetDateTime
 
 data class PapirSmRegistering(
-    val journalpostId: String,
-    val fnr: String?,
-    val aktorId: String?,
-    val dokumentInfoId: String?,
-    val datoOpprettet: LocalDateTime?,
-    val sykmeldingId: String,
-    val syketilfelleStartDato: LocalDate?,
-    val arbeidsgiver: Arbeidsgiver?,
-    val medisinskVurdering: MedisinskVurdering?,
-    val skjermesForPasient: Boolean?,
-    val perioder: List<Periode>?,
-    val prognose: Prognose?,
-    val utdypendeOpplysninger: Map<String, Map<String, SporsmalSvar>>?,
-    val tiltakNAV: String?,
-    val tiltakArbeidsplassen: String?,
-    val andreTiltak: String?,
-    val meldingTilNAV: MeldingTilNAV?,
-    val meldingTilArbeidsgiver: String?,
-    val kontaktMedPasient: KontaktMedPasient?,
-    val behandletTidspunkt: LocalDate?,
-    val behandler: Behandler?
+        val journalpostId: String,
+        val fnr: String?,
+        val aktorId: String?,
+        val dokumentInfoId: String?,
+        val datoOpprettet: OffsetDateTime?,
+        val sykmeldingId: String,
+        val syketilfelleStartDato: LocalDate?,
+        val arbeidsgiver: Arbeidsgiver?,
+        val medisinskVurdering: MedisinskVurdering?,
+        val skjermesForPasient: Boolean?,
+        val perioder: List<Periode>?,
+        val prognose: Prognose?,
+        val utdypendeOpplysninger: Map<String, Map<String, SporsmalSvar>>?,
+        val tiltakNAV: String?,
+        val tiltakArbeidsplassen: String?,
+        val andreTiltak: String?,
+        val meldingTilNAV: MeldingTilNAV?,
+        val meldingTilArbeidsgiver: String?,
+        val kontaktMedPasient: KontaktMedPasient?,
+        val behandletTidspunkt: LocalDate?,
+        val behandler: Behandler?
 )
--- src/test/kotlin/no/nav/syfo/service/PapirSmMapperServiceSpek.kt
+++ src/test/kotlin/no/nav/syfo/service/PapirSmMapperServiceSpek.kt
@@ -1,9 +1,6 @@
 package no.nav.syfo.service
 
 import io.ktor.util.KtorExperimentalAPI
-import java.io.StringReader
-import java.time.LocalDate
-import java.time.LocalDateTime
 import no.nav.helse.sykSkanningMeta.Skanningmetadata
 import no.nav.syfo.client.getFileAsString
 import no.nav.syfo.model.HarArbeidsgiver
@@ -15,6 +12,9 @@ import org.amshove.kluent.shouldBe
 import org.amshove.kluent.shouldEqual
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
+import java.io.StringReader
+import java.time.LocalDate
+import java.time.OffsetDateTime
 
 @KtorExperimentalAPI
 object PapirSmMapperServiceSpek : Spek({
@@ -23,8 +23,7 @@ object PapirSmMapperServiceSpek : Spek({
     val dokumentInfoId = "123"
     val fnrPasient = "12345678910"
     val aktorId = "aktorId"
-    val datoOpprettet = LocalDateTime.now()
-    val loggingMetadata = LoggingMeta(sykmeldingId, journalpostId, "hendelsesId")
+    val datoOpprettet = OffsetDateTime.now()
 
     describe("PapirSmMappingService") {
         it("Tests a complete OCR file, and should parse fine") {
