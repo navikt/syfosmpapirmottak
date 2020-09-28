a90dd6ec90de213c58f93046af172709776c53a0
Tia Firing <tia.firing@nav.no>
2020-09-28 13:01:01 +0200

    glemte parentes

--- src/main/kotlin/no/nav/syfo/service/BehandlingService.kt
+++ src/main/kotlin/no/nav/syfo/service/BehandlingService.kt
@@ -45,7 +45,7 @@ class BehandlingService(
 
             if (journalfoeringEvent.temaNytt.toString() == "SYM" &&
                     (journalfoeringEvent.mottaksKanal.toString() == "SKAN_NETS" || journalfoeringEvent.mottaksKanal.toString() == "SKAN_IM") &&
-                    journalfoeringEvent.hendelsesType.toString() == "MidlertidigJournalført" || journalfoeringEvent.hendelsesType.toString() == "TemaEndret"
+                (journalfoeringEvent.hendelsesType.toString() == "MidlertidigJournalført" || journalfoeringEvent.hendelsesType.toString() == "TemaEndret")
             ) {
                 val requestLatency = REQUEST_TIME.startTimer()
                 PAPIRSM_MOTTATT.inc()
