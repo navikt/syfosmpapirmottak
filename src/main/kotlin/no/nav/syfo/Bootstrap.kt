a1f174b6956709da50bb0d384338176ded57776a
Tia Firing <tia.firing@nav.no>
2022-01-12 15:30:28 +0100

    har mistet offset i dev

--- src/main/kotlin/no/nav/syfo/Bootstrap.kt
+++ src/main/kotlin/no/nav/syfo/Bootstrap.kt
@@ -95,7 +95,7 @@ fun main() {
     DefaultExports.initialize()
 
     val kafkaBaseConfig = loadBaseConfig(env, credentials).envOverrides()
-    kafkaBaseConfig["auto.offset.reset"] = "none"
+    kafkaBaseConfig["auto.offset.reset"] = "latest"
 
     val consumerProperties = kafkaBaseConfig.toConsumerConfig(
         "${env.applicationName}-consumer-v2",
