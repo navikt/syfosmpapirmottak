086fe1d3d014ed76b13ee8dff8fe9667e519339d
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2023-04-12 08:20:44 +0200

    Upgrade shadow to 8.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,7 @@ plugins {
     java
     kotlin("jvm") version "1.8.20"
     id("com.diffplug.spotless") version "6.18.0"
-    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
     id("com.apollographql.apollo") version "2.5.14"
     id("org.jmailen.kotlinter") version "3.14.0"
 }
